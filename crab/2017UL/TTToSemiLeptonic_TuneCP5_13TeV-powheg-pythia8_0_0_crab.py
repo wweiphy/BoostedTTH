4f343e9836638c0f3e480b6017d9af47fe75eeb5
wweiphy <wei.w@cern.ch>
2022-10-12 10:04:28 -0500

    test crab

---
+++ crab/2017UL/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8_0_0_crab.py
@@ -0,0 +1,30 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+config.General.requestName = 'TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8_new_pmx_2017_ntuple_0_0'
+config.General.workArea = 'crab_ntuple'
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = '/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_10_6_27/src/BoostedTTH/BoostedAnalyzer/test//boostedAnalysis_ntuples-Legacy_2016_2017_2018_cfg_UL.py'
+config.JobType.outputFiles = ["ntuples_nominal_Tree.root", "ntuples_nominal_Cutflow.txt"]
+# config.JobType.maxJobRuntimeMin = 2800
+config.JobType.maxMemoryMB = 20000
+config.JobType.numCores = 8
+config.JobType.maxMemoryMB = 4000
+config.JobType.pyCfgParams = ['isData=FALSE','maxEvents=999999999','outName=ntuples', 'dataEra=2017','systematicVariations=nominal','weight=2.78E-05','ProduceMemNtuples=False', 'deterministicSeeds=False']
+config.JobType.sendPythonFolder=True
+config.JobType.allowUndistributedCMSSW = True
+
+config.Data.inputDataset = '/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL17MiniAODv2-PUForMUOVal_106X_mc2017_realistic_v9_ext1-v2/MINIAODSIM'
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'EventAwareLumiBased'
+config.Data.unitsPerJob = 1500
+# config.Data.splitting = 'FileBased'
+config.Data.publication = False
+config.Data.publishDBS = 'phys03'
+config.Data.outputDatasetTag = 'sl_LEG_ntuple_2017'
+config.Data.outLFNDirBase = '/store/group/lpctthrun2/wwei/UL/2017'
+
+
+config.Site.storageSite = 'T3_US_FNALLPC'
+
