6a982d886690444d2e5a4c255c6aed32d47cdc71
wweiphy <wei.w@cern.ch>
2022-10-11 23:38:29 -0500

    fixup

--- crab/2017UL/TTHHTo4b_TuneCP5_13TeV-madgraph-pythia8_0_0_crab.py
+++ crab/2017UL/TTHHTo4b_TuneCP5_13TeV-madgraph-pythia8_0_0_crab.py
@@ -16,7 +16,7 @@ config.JobType.sendPythonFolder=True
 config.JobType.allowUndistributedCMSSW = True
 
 config.Data.inputDataset = '/TTHHTo4b_TuneCP5_13TeV-madgraph-pythia8/RunIISummer20UL17MiniAODv2-106X_mc2017_realistic_v9-v2/MINIAODSIM'
-config.Data.inputDBS = 'phys03'
+config.Data.inputDBS = 'global'
 config.Data.unitsPerJob = 1500
 config.Data.splitting = 'EventAwareLumiBased'
 #config.Data.unitsPerJob = 1
