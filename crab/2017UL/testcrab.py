e1a3dc1fb7748a686020c96c5e024b5f6caf1bb8
wweiphy <wei.w@cern.ch>
2022-10-13 15:27:03 -0500

    updates

--- crab/2017UL/testcrab.py
+++ crab/2017UL/testcrab.py
@@ -3,17 +3,14 @@ from CRABClient.UserUtilities import config
 
 config = config()
 
-config.General.requestName = 'TTHHTo4b_TuneCP5_13TeV-madgraph-pythia8_2017_ntuple_0_0'
-# config.General.workArea = '2017'
+config.General.requestName = 'TTHHTo4b'
 
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'pset.py'
-# config.JobType.psetName = '/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_10_6_29/src/BoostedTTH/BoostedAnalyzer/test/boostedAnalysis_ntuples-Legacy_2016_2017_2018_cfg_UL.py'
 config.JobType.outputFiles = ["ntuples_nominal_Tree.root", "ntuples_nominal_Cutflow.txt"]
 # config.JobType.maxJobRuntimeMin = 2800
 # config.JobType.maxMemoryMB = 20000
 # config.JobType.numCores = 8
-#config.JobType.maxMemoryMB = 4000
 config.JobType.pyCfgParams = ['isData=FALSE','maxEvents=999999999','outName=ntuples', 'dataEra=2017','systematicVariations=nominal','weight=2.67E-08','ProduceMemNtuples=False', 'deterministicSeeds=False']
 config.JobType.sendPythonFolder=True
 config.JobType.allowUndistributedCMSSW = True
@@ -27,10 +24,10 @@ config.Data.splitting = 'FileBased'
 
 #config.Data.unitsPerJob = 720
 #config.Data.splitting = 'Automatic'
-config.Data.publication = True
+config.Data.publication = False
 config.Data.publishDBS = 'phys03'
 config.Data.outputDatasetTag = 'sl_LEG_ntuple_2017'
-config.Data.outLFNDirBase = '/store/group/lpctthrun2/wwei/UL/2017'
+# config.Data.outLFNDirBase = '/store/group/lpctthrun2/wwei/UL/2017'
 
 
 config.Site.storageSite = 'T3_US_FNALLPC'
