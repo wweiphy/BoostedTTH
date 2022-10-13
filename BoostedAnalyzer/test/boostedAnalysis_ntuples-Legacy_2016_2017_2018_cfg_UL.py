424ab9a3f2ff86223e1c3c7cf229037346b68f45
wweiphy <wei.w@cern.ch>
2022-10-12 23:39:20 -0500

    test

--- BoostedAnalyzer/test/boostedAnalysis_ntuples-Legacy_2016_2017_2018_cfg_UL.py
+++ BoostedAnalyzer/test/boostedAnalysis_ntuples-Legacy_2016_2017_2018_cfg_UL.py
@@ -156,7 +156,7 @@ process.load("CondCore.CondDB.CondDB_cfi")
 process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(False) )
 process.options.allowUnscheduled = cms.untracked.bool(False)
 
-# process.options.numberOfThreads=cms.untracked.uint32(8)
+process.options.numberOfThreads=cms.untracked.uint32(8)
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(int(options.maxEvents)))
 process.source = cms.Source(  "PoolSource",
--- crab/2017UL/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8_0_0_crab.py
+++ crab/2017UL/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8_0_0_crab.py
@@ -8,8 +8,8 @@ config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = '/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_10_6_27/src/BoostedTTH/BoostedAnalyzer/test//boostedAnalysis_ntuples-Legacy_2016_2017_2018_cfg_UL.py'
 config.JobType.outputFiles = ["ntuples_nominal_Tree.root", "ntuples_nominal_Cutflow.txt"]
 # config.JobType.maxJobRuntimeMin = 2800
-# config.JobType.maxMemoryMB = 20000
-# config.JobType.numCores = 8
+config.JobType.maxMemoryMB = 20000
+config.JobType.numCores = 8
 # config.JobType.maxMemoryMB = 4000
 config.JobType.pyCfgParams = ['isData=FALSE','maxEvents=999999999','outName=ntuples', 'dataEra=2017','systematicVariations=nominal','weight=2.78E-05','ProduceMemNtuples=False', 'deterministicSeeds=False']
 config.JobType.sendPythonFolder=True
@@ -17,9 +17,9 @@ config.JobType.allowUndistributedCMSSW = True
 
 config.Data.inputDataset = '/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL17MiniAODv2-106X_mc2017_realistic_v9-v1/MINIAODSIM'
 config.Data.inputDBS = 'global'
-config.Data.splitting = 'EventAwareLumiBased'
-config.Data.unitsPerJob = 8000
-# config.Data.splitting = 'FileBased'
+# config.Data.splitting = 'EventAwareLumiBased'
+config.Data.unitsPerJob = 5
+config.Data.splitting = 'FileBased'
 config.Data.publication = False
 config.Data.publishDBS = 'phys03'
 config.Data.outputDatasetTag = 'sl_LEG_ntuple_2017'
