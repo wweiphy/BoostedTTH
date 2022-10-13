0763431894c9dbd2070b8bbbee5e74cab01bdedf
wweiphy <wei.w@cern.ch>
2022-10-13 14:55:28 -0500

    updates

--- crab/2017UL/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8_0_0_crab.py
+++ crab/2017UL/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8_0_0_crab.py
@@ -2,7 +2,8 @@ from CRABClient.UserUtilities import config
 config = config()
 
 config.General.requestName = 'TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8_2017_ntuple_0_0'
-# config.General.workArea = '2017'
+config.General.workArea = 'crab_ntuple'
+config.General.transferOutputs = True 
 
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = '/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_10_6_29/src/BoostedTTH/BoostedAnalyzer/test/boostedAnalysis_ntuples-Legacy_2016_2017_2018_cfg_UL.py'
@@ -10,7 +11,6 @@ config.JobType.outputFiles = ["ntuples_nominal_Tree.root", "ntuples_nominal_Cutf
 # config.JobType.maxJobRuntimeMin = 2800
 # config.JobType.maxMemoryMB = 20000
 # config.JobType.numCores = 8
-# config.JobType.maxMemoryMB = 4000
 config.JobType.pyCfgParams = ['isData=FALSE','maxEvents=999999999','outName=ntuples', 'dataEra=2017','systematicVariations=nominal','weight=2.78E-05','ProduceMemNtuples=False', 'deterministicSeeds=False']
 config.JobType.sendPythonFolder=True
 config.JobType.allowUndistributedCMSSW = True
