520743b044ec8f5c49dcb9d053c73389cd3e50b1
wweiphy <wei.w@cern.ch>
2022-11-02 20:50:19 -0500

    test

--- crab/2017UL/ntuple/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8_0_0_crab.py
+++ crab/2017UL/ntuple/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8_0_0_crab.py
@@ -16,10 +16,11 @@ config.JobType.sendPythonFolder=True
 config.JobType.allowUndistributedCMSSW = True
 
 config.Data.inputDataset = '/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8/lpctthrun2-sl_skims_MC_LEG_2017-f7a1084d3f7c1cbe4d4074d5b1a88d52/USER'
+
 config.Data.inputDBS = 'phys03'
 config.Data.splitting = 'EventAwareLumiBased'
-config.Data.unitsPerJob = 5000
-# NJOB = 9000
+config.Data.unitsPerJob = 15000
+# NJOB = 9500
 # config.Data.totalUnits = config.Data.unitsPerJob * NJOB
 # config.Data.splitting = 'FileBased'
 # config.Data.unitsPerJob = 360
