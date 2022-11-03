d7e7fa423394ae3f5dc258ebd2289519b3dda6e5
wweiphy <wei.w@cern.ch>
2022-11-02 19:11:46 -0500

    updates

--- crab/2017UL/ntuple/TTHHTo4b_5f_LO_TuneCP5_13TeV_madgraph_pythia8_0_0_crab.py
+++ crab/2017UL/ntuple/TTHHTo4b_5f_LO_TuneCP5_13TeV_madgraph_pythia8_0_0_crab.py
@@ -29,8 +29,8 @@ config.Data.splitting = 'EventAwareLumiBased'
 config.Data.publication = False
 config.Data.publishDBS = 'phys03'
 config.Data.outputDatasetTag = 'sl_LEG_ntuple_2017'
-# config.Data.outLFNDirBase = '/store/group/lpctthrun2/wwei/UL/2017/ntuple'
-config.Data.outLFNDirBase = '/store/user/wwei/UL/2017/ntuple'
+config.Data.outLFNDirBase = '/store/group/lpctthrun2/wwei/UL/2017/ntuple'
+# config.Data.outLFNDirBase = '/store/user/wwei/UL/2017/ntuple'
 
 
 config.Site.storageSite = 'T3_US_FNALLPC'
