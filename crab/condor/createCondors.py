e47e5cbb7c750752b3ab44ef5497aabae4ad7fe8
wweiphy <wei.w@cern.ch>
2022-11-04 17:16:17 -0500

    updates

--- crab/condor/createCondors.py
+++ crab/condor/createCondors.py
@@ -24,6 +24,9 @@ parser.add_option("-c", "--cpus", dest="cpus", default=1,
 parser.add_option("-m", "--memory", dest="memory", default=20000,
                   help="Memory request", metavar="memory")
 
+parser.add_option("-n", "--numberfile", dest="numberfile", default=50,
+                  help="Number of files per job", metavar="numberfile")
+
 # parser.add_option("-d", "--disk", dest="disk", default=5000,
                 #   help="Disk request", metavar="disk")
 
@@ -60,60 +63,115 @@ environment = Environment(loader=FileSystemLoader("."))
 scriptTemplate = environment.get_template("condor_template.sh")
 jdlTemplate = environment.get_template("condor_template.jdl")
 
+OUTDIR = "root://cmseos.fnal.gov//store/user/wwei/UL/2017/ttSL"
+
 # eospath = "root://cmseos.fnal.gov/"
 
 for i, file in enumerate(allFiles):
 
+    inputfile = file.replace("/eos/uscms", "")
 
     # INFILE = "xrdcp -f " + eospath + file + " ."
+    if i % options.numberfile == 0:
+        allinputfile = "inputFiles=" + inputfile
     
-    inputfile = file.replace("/eos/uscms", "")
+    else:
+        allinputfile += " inputFiles=" + inputfile
     
-    ROOTOUTFILE = "ntuples_{}_nominal_Tree.root".format(i)
-    TXTOUTFILE = "ntuples_{}_nominal_Cutflow.txt".format(i)
-    OUTDIR = "root://cmseos.fnal.gov//store/user/wwei/UL/2017/ttSL"
+    if (i+1) % options.numberfile == 0:
+        k = (i+1)/options.numberfile
+        print("run with every {} files, {} times".format(options.numberfile, k))
 
-    # xrdcp - f {{EVEN_OUTFILE}} {{OUTDIR}}/{{EVEN_OUTFILE}}
-    TRANSFEROUTFILE = "xrdcp -f " + ROOTOUTFILE + " " + OUTDIR + "/" + ROOTOUTFILE + \
-        "\n" + "xrdcp -f " + TXTOUTFILE + " " + OUTDIR + "/" + TXTOUTFILE + "\n"
+        ROOTOUTFILE = "ntuples_{}_nominal_Tree.root".format(k)
+        TXTOUTFILE = "ntuples_{}_nominal_Cutflow.txt".format(k)
+        
+        TRANSFEROUTFILE = "xrdcp -f " + ROOTOUTFILE + " " + OUTDIR + "/" + ROOTOUTFILE + \
+            "\n" + "xrdcp -f " + TXTOUTFILE + " " + OUTDIR + "/" + TXTOUTFILE + "\n"
 
-    RUNCOMMAND = "cmsRun boostedAnalysis_ntuples-Legacy_2016_2017_2018_cfg_UL.py isData=False outName=ntuples_{} maxEvents=99999999 systematicVariations=nominal dataEra=2017 ProduceMemNtuples=False deterministicSeeds=False weight=3.42E-06 systematicVariations=nominal inputFiles=".format(i) + inputfile
+        RUNCOMMAND = "cmsRun boostedAnalysis_ntuples-Legacy_2016_2017_2018_cfg_UL.py isData=False outName=ntuples_{} maxEvents=999999999 systematicVariations=nominal dataEra=2017 ProduceMemNtuples=False deterministicSeeds=False weight=3.42E-06 systematicVariations=nominal inputFiles=".format(k) + allinputfile
 
-    scriptFileName = outPath + "/" + options.process + "_ntuple_{}.sh".format(i)
-    scriptcontent = scriptTemplate.render(
-        # SCRAM_ARCH = SCRAM_ARCH,
-        # CMSSW_VERSION = CMSSW_VERSION,
-        SETUP = SETUP,
-        # INFILE = INFILE,
-        RUNCOMMAND = RUNCOMMAND,
-        # OUTDIR = OUTDIR,
-        TRANSFEROUTFILE=TRANSFEROUTFILE,
-        # EVEN_OUTFILE = EVEN_OUTFILE,
-        # ODD_OUTFILE = ODD_OUTFILE,
-        DELETE = DELETE
-    )
-    with open(scriptFileName, mode="w") as scriptFile:
-        scriptFile.write(scriptcontent)
-        print("... wrote {}".format(scriptFileName))
+        scriptFileName = outPath + "/" + options.process + "_ntuple_{}.sh".format(k)
+        scriptcontent = scriptTemplate.render(
+            # SCRAM_ARCH = SCRAM_ARCH,
+            # CMSSW_VERSION = CMSSW_VERSION,
+            SETUP = SETUP,
+            # INFILE = INFILE,
+            RUNCOMMAND = RUNCOMMAND,
+            # OUTDIR = OUTDIR,
+            TRANSFEROUTFILE=TRANSFEROUTFILE,
+            # EVEN_OUTFILE = EVEN_OUTFILE,
+            # ODD_OUTFILE = ODD_OUTFILE,
+            DELETE = DELETE
+        )
+        with open(scriptFileName, mode="w") as scriptFile:
+            scriptFile.write(scriptcontent)
+            print("... wrote {}".format(scriptFileName))
 
-    # for jdl file
+        # for jdl file
 
-    EXECUTABLE = scriptFileName
-    FILES = scriptFileName
-    OUTFILES = scriptFileName.split(".")[0]
+        EXECUTABLE = scriptFileName
+        FILES = scriptFileName
+        OUTFILES = scriptFileName.split(".")[0]
 
-    jdlFileName = outPath + "/" + options.process + "_ntuple_{}.jdl".format(i)
-    jdlcontent = jdlTemplate.render(
-        MEMORY = MEMORY,
-        # DISK = DISK,
-        CPU = CPU,
-        EXECUTABLE = EXECUTABLE,
-        FILES = FILES,
-        OUTFILES = OUTFILES
-    )
-    with open(jdlFileName, mode="w") as jdlFile:
-        jdlFile.write(jdlcontent)
-        print("... wrote {}".format(jdlFileName))
+        jdlFileName = outPath + "/" + options.process + "_ntuple_{}.jdl".format(k)
+        jdlcontent = jdlTemplate.render(
+            MEMORY = MEMORY,
+            # DISK = DISK,
+            CPU = CPU,
+            EXECUTABLE = EXECUTABLE,
+            FILES = FILES,
+            OUTFILES = OUTFILES
+        )
+        with open(jdlFileName, mode="w") as jdlFile:
+            jdlFile.write(jdlcontent)
+            print("... wrote {}".format(jdlFileName))
 
+    if i == len(allFiles) - 1:
+        k = i / options.numberfile + 1
+        print("run with the last set of files")
 
-    
+        ROOTOUTFILE = "ntuples_{}_nominal_Tree.root".format(k)
+        TXTOUTFILE = "ntuples_{}_nominal_Cutflow.txt".format(k)
+
+        TRANSFEROUTFILE = "xrdcp -f " + ROOTOUTFILE + " " + OUTDIR + "/" + ROOTOUTFILE + \
+            "\n" + "xrdcp -f " + TXTOUTFILE + " " + OUTDIR + "/" + TXTOUTFILE + "\n"
+
+        RUNCOMMAND = "cmsRun boostedAnalysis_ntuples-Legacy_2016_2017_2018_cfg_UL.py isData=False outName=ntuples_{} maxEvents=999999999 systematicVariations=nominal dataEra=2017 ProduceMemNtuples=False deterministicSeeds=False weight=3.42E-06 systematicVariations=nominal inputFiles=".format(k) + allinputfile
+
+        scriptFileName = outPath + "/" + \
+            options.process + "_ntuple_{}.sh".format(k)
+        scriptcontent = scriptTemplate.render(
+            # SCRAM_ARCH = SCRAM_ARCH,
+            # CMSSW_VERSION = CMSSW_VERSION,
+            SETUP=SETUP,
+            # INFILE = INFILE,
+            RUNCOMMAND=RUNCOMMAND,
+            # OUTDIR = OUTDIR,
+            TRANSFEROUTFILE=TRANSFEROUTFILE,
+            # EVEN_OUTFILE = EVEN_OUTFILE,
+            # ODD_OUTFILE = ODD_OUTFILE,
+            DELETE=DELETE
+        )
+        with open(scriptFileName, mode="w") as scriptFile:
+            scriptFile.write(scriptcontent)
+            print("... wrote {}".format(scriptFileName))
+
+        # for jdl file
+
+        EXECUTABLE = scriptFileName
+        FILES = scriptFileName
+        OUTFILES = scriptFileName.split(".")[0]
+
+        jdlFileName = outPath + "/" + \
+            options.process + "_ntuple_{}.jdl".format(k)
+        jdlcontent = jdlTemplate.render(
+            MEMORY=MEMORY,
+            # DISK = DISK,
+            CPU=CPU,
+            EXECUTABLE=EXECUTABLE,
+            FILES=FILES,
+            OUTFILES=OUTFILES
+        )
+        with open(jdlFileName, mode="w") as jdlFile:
+            jdlFile.write(jdlcontent)
+            print("... wrote {}".format(jdlFileName))
