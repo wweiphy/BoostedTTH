7d9290688130be59cfaf61f18139345de68ec8d4
wweiphy <wei.w@cern.ch>
2022-11-21 21:20:33 -0600

    test

--- crab/2017UL/ntuple/TTHHTo4b_5f_LO_TuneCP5_13TeV_madgraph_pythia8_0_0_crab.py
+++ crab/2017UL/ntuple/TTHHTo4b_5f_LO_TuneCP5_13TeV_madgraph_pythia8_0_0_crab.py
@@ -13,7 +13,7 @@ config.JobType.outputFiles = ["ntuples_nominal_Tree.root", "ntuples_nominal_Cutf
 config.JobType.maxMemoryMB = 4000
 # config.JobType.numCores = 8
 
-config.JobType.pyCfgParams = ['isData=FALSE', 'maxEvents=999999999', 'outName=ntuples', 'dataEra=2017', 'systematicVariations = nominal, JES, JER, JESFlavorQCD, JESRelativeBal, JESHF, JESBBEC1, JESEC2, JESAbsolute, JESBBEC1year, JESRelativeSampleyear, JESEC2year, JESHFyear, JESAbsoluteyear', 'weight=2.65E-08', 'ProduceMemNtuples=False', 'deterministicSeeds=False']
+config.JobType.pyCfgParams = ['isData=FALSE', 'maxEvents=999999999', 'outName=ntuples', 'dataEra=2017', 'systematicVariations = nominal,JES,JER,JESFlavorQCD,JESRelativeBal,JESHF,JESBBEC1,JESEC2,JESAbsolute,JESBBEC1year,JESRelativeSampleyear,JESEC2year,JESHFyear,JESAbsoluteyear', 'weight=2.65E-08', 'ProduceMemNtuples=False', 'deterministicSeeds=False']
 config.JobType.sendPythonFolder=True
 config.JobType.allowUndistributedCMSSW = True
 
