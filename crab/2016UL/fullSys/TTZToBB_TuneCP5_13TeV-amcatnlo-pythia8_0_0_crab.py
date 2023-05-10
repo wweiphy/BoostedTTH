6ecf73341444f8298e28ed28b6f29b02fd6e2cb6
wweiphy <wei.w@cern.ch>
2023-05-10 11:57:32 -0500

    update

--- crab/2016UL/fullSys/TTZToBB_TuneCP5_13TeV-amcatnlo-pythia8_0_0_crab.py
+++ crab/2016UL/fullSys/TTZToBB_TuneCP5_13TeV-amcatnlo-pythia8_0_0_crab.py
@@ -1,7 +1,8 @@
 from CRABClient.UserUtilities import config
 config = config()
 
-config.General.requestName = 'TTZToBB_TuneCP5_13TeV-amcatnlo-pythia8_2016postVFP_ntuple_0_0_2'
+config.General.requestName = 'TTZToBB_TuneCP5_13TeV-amcatnlo-pythia8_2016postVFP_ntuple_0_0'
+
 config.General.workArea = 'crab_ntuple'
 
 config.JobType.pluginName = 'Analysis'
