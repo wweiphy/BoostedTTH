bddd9f2eac1aa9bf96d4140e8719163fb036d702
wweiphy <wei.w@cern.ch>
2023-05-18 11:49:31 -0500

    updates

--- crab/2016UL-APV/fullSys/TT4b_TuneCP5_13TeV_madgraph_pythia8_0_0_crab.py
+++ crab/2016UL-APV/fullSys/TT4b_TuneCP5_13TeV_madgraph_pythia8_0_0_crab.py
@@ -1,7 +1,7 @@
 from CRABClient.UserUtilities import config
 config = config()
 
-config.General.requestName = 'TT4b_TuneCP5_13TeV_madgraph_pythia8_2016preVFP_ntuple_0_0'
+config.General.requestName = 'TT4b_TuneCP5_13TeV_madgraph_pythia8_2016preVFP_ntuple_0_0_2'
 config.General.workArea = 'crab_ntuple'
 
 config.JobType.pluginName = 'Analysis'
