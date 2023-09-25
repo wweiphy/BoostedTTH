54d3797bd978520312f528f399bfb5d0d5c3a9dd
wweiphy <wei.w@cern.ch>
2023-09-25 15:16:20 -0500

    update

--- crab/2017UL/ntuple-ControlRegion/SingleMuonD_0_0_crab.py
+++ crab/2017UL/ntuple-ControlRegion/SingleMuonD_0_0_crab.py
@@ -1,7 +1,7 @@
 from CRABClient.UserUtilities import config
 config = config()
 
-config.General.requestName = 'SingleMuonD_2017D_ntuple_0_0'
+config.General.requestName = 'SingleMuonD_2017D_ntuple_0_0_2'
 config.General.workArea = 'crab_ntuple'
 
 config.JobType.pluginName = 'Analysis'
