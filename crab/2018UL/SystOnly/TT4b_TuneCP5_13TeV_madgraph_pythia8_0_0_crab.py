3fc31576e7e0336f0ffcb2aab9938b4da3694d97
wweiphy <wei.w@cern.ch>
2024-03-12 10:52:10 -0500

    update

---
+++ crab/2018UL/SystOnly/TT4b_TuneCP5_13TeV_madgraph_pythia8_0_0_crab.py
@@ -0,0 +1,40 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+config.General.requestName = 'TT4b_TuneCP5_13TeV_madgraph_pythia8_2018_ntuple_0_0'
+config.General.workArea = 'crab_ntuple'
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = '/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_10_6_29/src/BoostedTTH/BoostedAnalyzer/test/boostedAnalysis_ntuples-Legacy_2016_2017_2018_cfg_UL.py'
+
+# config.JobType.outputFiles = ["ntuples_nominal_Tree.root", "ntuples_nominal_Cutflow.txt"]
+config.JobType.outputFiles = ["ntuples_nominal_Tree.root", "ntuples_nominal_Cutflow.txt", "ntuples_JESup_Tree.root", "ntuples_JESup_Cutflow.txt", "ntuples_JESdown_Tree.root", "ntuples_JESdown_Cutflow.txt", "ntuples_JERup_Tree.root", "ntuples_JERup_Cutflow.txt", "ntuples_JERdown_Tree.root", "ntuples_JERdown_Cutflow.txt"]
+                              
+config.JobType.maxJobRuntimeMin = 2750
+config.JobType.maxMemoryMB = 20000
+config.JobType.numCores = 8
+# config.JobType.maxMemoryMB = 4000
+config.JobType.pyCfgParams = ['isData=FALSE', 'maxEvents=999999999', 'outName=ntuples', 'dataEra=2018',
+                              'systematicVariations=nominal,JES,JER', 'weight=3.01E-05', 'ProduceMemNtuples=False', 'deterministicSeeds=False']
+
+# config.JobType.pyCfgParams = ['isData=FALSE','maxEvents=999999999','outName=ntuples', 'dataEra=2018','systematicVariations=nominal','weight=3.12E-05','ProduceMemNtuples=False', 'deterministicSeeds=False']
+
+config.JobType.sendPythonFolder=True
+config.JobType.allowUndistributedCMSSW = True
+
+config.Data.inputDataset = '/TT4b_TuneCP5_13TeV_madgraph_pythia8/RunIISummer20UL18MiniAODv2-106X_upgrade2018_realistic_v16_L1v1-v2/MINIAODSIM'
+config.Data.inputDBS = 'global'
+# config.Data.unitsPerJob = 1000
+# config.Data.splitting = 'EventAwareLumiBased'
+#config.Data.splitting = 'Automatic'
+config.Data.unitsPerJob = 2
+config.Data.splitting = 'FileBased'
+config.Data.publication = False
+config.Data.publishDBS = 'phys03'
+config.Data.outputDatasetTag = 'sl_LEG_ntuple_2018'
+config.Data.outLFNDirBase = '/store/group/lpctthrun2/wwei/UL/2018/ntuple'
+# config.Data.outLFNDirBase = '/store/user/wwei/UL/2017/ntuple'
+
+
+config.Site.storageSite = 'T3_US_FNALLPC'
+
