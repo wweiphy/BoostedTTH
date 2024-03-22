a5bed04956600c88dad67032107d516680609862
wweiphy <wei.w@cern.ch>
2024-03-21 22:22:10 -0500

    update

--- BoostedAnalyzer/python/Selection_cff.py
+++ BoostedAnalyzer/python/Selection_cff.py
@@ -89,7 +89,7 @@ DiLeptonMETSelection = cms.PSet(
 
 
 JetTagSelection = cms.PSet(
-    minJets = cms.vint32(4),
+    minJets = cms.vint32(5),
     maxJets = cms.vint32(-1),
     minTags = cms.vint32(-1),
     maxTags = cms.vint32(-1)
