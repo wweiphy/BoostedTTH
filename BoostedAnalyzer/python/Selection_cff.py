78ccf0dcd3855ac1d8038e1b60ac00a154258f5d
wweiphy <wei.w@cern.ch>
2024-03-25 09:44:12 -0500

    update

--- BoostedAnalyzer/python/Selection_cff.py
+++ BoostedAnalyzer/python/Selection_cff.py
@@ -89,9 +89,9 @@ DiLeptonMETSelection = cms.PSet(
 
 
 JetTagSelection = cms.PSet(
-    minJets = cms.vint32(5),
+    minJets = cms.vint32(4),
     maxJets = cms.vint32(-1),
-    minTags = cms.vint32(-1),
+    minTags = cms.vint32(3),
     maxTags = cms.vint32(-1)
 ) # for control region study
 
