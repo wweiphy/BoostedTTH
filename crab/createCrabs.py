3a8d6e3ad616c537866985f31b1d007107c50d38
wweiphy <wei.w@cern.ch>
2024-03-25 21:00:18 -0500

    update

--- crab/createCrabs.py
+++ crab/createCrabs.py
@@ -141,8 +141,6 @@ for row in reader:
             elif row['run'] == "2016preVFP" or row['run'] == "2016postVFP":
                 lumimask = 'https://cms-service-dqmdc.web.cern.ch/CAF/certification/Collisions16/13TeV/Legacy_2016/Cert_271036-284044_13TeV_Legacy2016_Collisions16_JSON.txt'
 
-            repl('LUMIMASK',lumimask,out)
-
         variations_list = split_for_systematic_variations(variation_list,options.nsysts)
 
         for variations,l in zip(variations_list,range(len(variations_list))):
@@ -203,5 +201,8 @@ for row in reader:
                 repl('WORKAREA',workarea,out)
                 repl('DBSINSTANCE',dbsinstance,out)
                 repl('OUTNAME',cmsoutname,out)
+
+                if row['isData']=='True':
+                    repl('LUMIMASK',lumimask,out)
 
 
