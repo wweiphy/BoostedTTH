adcf6439ffde267d2d64d05117a54f98e6e1b510
wweiphy <wei.w@cern.ch>
2024-03-25 21:25:55 -0500

    update

--- crab/createCrabs.py
+++ crab/createCrabs.py
@@ -117,7 +117,7 @@ lumimask = ''
 for row in reader:
     if not ("#" or "") in row["name"]:
         #print variation_list
-        if row['isData']=='False':
+        if row['isData']=='FALSE':
             if ntuple and not options.systematics:
                 src='common/template_cfg_ntuple.py'
                 datasets=row['boosted_dataset'].split(",")
