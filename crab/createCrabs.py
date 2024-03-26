0864c285e4e52f63686a03523834ca22d593c666
wweiphy <wei.w@cern.ch>
2024-03-25 21:10:44 -0500

    update

--- crab/createCrabs.py
+++ crab/createCrabs.py
@@ -134,10 +134,10 @@ for row in reader:
             variation_list = get_list_of_systematics("common/systematicVariationsNone.txt")
             datasets=row['boosted_dataset'].split(",")
             print("Creating crab configs to Ntuple with systs, therefore using common/systematicVariations_new.txt")
-
-            if row['run'] == 2018:
+            print(row['run'])
+            if row['run'] == '2018':
                 lumimask = 'https://cms-service-dqmdc.web.cern.ch/CAF/certification/Collisions18/13TeV/Legacy_2018/Cert_314472-325175_13TeV_Legacy2018_Collisions18_JSON.txt'
-            elif row['run'] == 2017:
+            elif row['run'] == '2017':
                 lumimask = 'https://cms-service-dqmdc.web.cern.ch/CAF/certification/Collisions17/13TeV/Legacy_2017/Cert_294927-306462_13TeV_UL2017_Collisions17_GoldenJSON.txt'
             elif row['run'] == "2016preVFP" or row['run'] == "2016postVFP":
                 lumimask = 'https://cms-service-dqmdc.web.cern.ch/CAF/certification/Collisions16/13TeV/Legacy_2016/Cert_271036-284044_13TeV_Legacy2016_Collisions16_JSON.txt'
