f512dd528b8610169353eea0f168b4db3b1b93d1
wweiphy <wei.w@cern.ch>
2024-04-27 10:28:21 -0500

    update

--- crab/2018UL/SystOnly/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8_0_0_crab.py
+++ crab/2018UL/SystOnly/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8_0_0_crab.py
@@ -1,7 +1,7 @@
 from CRABClient.UserUtilities import config
 config = config()
 
-config.General.requestName = 'TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8_2018_ntuple_0_0_4'
+config.General.requestName = 'TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8_2018_ntuple_0_0_5'
 config.General.workArea = 'crab_ntuple'
 
 config.JobType.pluginName = 'Analysis'
