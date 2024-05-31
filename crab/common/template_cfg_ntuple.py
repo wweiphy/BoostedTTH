feb3cb25b00d0136a053baaf1db2c1d27159623b
wweiphy <wei.w@cern.ch>
2024-05-30 23:10:47 -0500

    update

--- crab/common/template_cfg_ntuple.py
+++ crab/common/template_cfg_ntuple.py
@@ -7,7 +7,7 @@ config.General.workArea = 'WORKAREA'
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'CMSSWPATH/RUNCONFIG'
 config.JobType.outputFiles = OUTPUTFILES
-config.JobType.maxJobRuntimeMin = 1200
+# config.JobType.maxJobRuntimeMin = 1200
 config.JobType.maxMemoryMB = 10000 
 config.JobType.numCores = 4
 config.JobType.pyCfgParams = ['isData=ISDATA','maxEvents=999999999','outName=OUTNAME', 'dataEra=DATAERA','systematicVariations=SYSTEMATICVARIATIONS','weight=WEIGHT','ProduceMemNtuples=False', 'deterministicSeeds=False']
@@ -16,9 +16,9 @@ config.JobType.pyCfgParams = ['isData=ISDATA','maxEvents=999999999','outName=OUT
 config.Data.inputDataset = 'THEINPUTDATASET'
 config.Data.inputDBS = 'DBSINSTANCE'
 # config.Data.splitting = 'EventAwareLumiBased'
-config.Data.unitsPerJob = 15 # Trigger
-config.Data.splitting = 'SPLITTING'
-# config.Data.splitting = 'Automatic'
+# config.Data.unitsPerJob = 15 # Trigger
+# config.Data.splitting = 'SPLITTING'
+config.Data.splitting = 'Automatic'
 config.Data.publication = False
 config.Data.publishDBS = 'phys03'
 config.Data.outputDatasetTag = 'sl_LEG_NTUPLETAG_DATAERA'
@@ -26,6 +26,6 @@ config.Data.outputDatasetTag = 'sl_LEG_NTUPLETAG_DATAERA'
 config.Data.outLFNDirBase = '/store/group/lpctthrun2/wwei/UL/DATAERA/ntuple'
 
 config.Site.storageSite = 'T3_US_FNALLPC'
-config.Site.blacklist = ['T2_CH_CERN','T2_US_Wisconsin','T2_US_Purdue','T2_US_Nebraska']
+# config.Site.blacklist = ['T2_CH_CERN','T2_US_Wisconsin','T2_US_Purdue','T2_US_Nebraska']
 
 
