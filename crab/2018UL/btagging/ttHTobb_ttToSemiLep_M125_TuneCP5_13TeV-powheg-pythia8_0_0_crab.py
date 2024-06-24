5839c29f1c9a5e5784a9c99825d5371b9c1633a4
wweiphy <wei.w@cern.ch>
2024-06-24 15:27:27 -0500

    Update

--- crab/2018UL/btagging/ttHTobb_ttToSemiLep_M125_TuneCP5_13TeV-powheg-pythia8_0_0_crab.py
+++ crab/2018UL/btagging/ttHTobb_ttToSemiLep_M125_TuneCP5_13TeV-powheg-pythia8_0_0_crab.py
@@ -5,14 +5,14 @@ config.General.requestName = 'ttHTobb_ttToSemiLep_M125_TuneCP5_13TeV-powheg-pyth
 config.General.workArea = 'crab_ntuple'
 
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = '/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_10_6_29/src/BoostedTTH/BoostedAnalyzer/test/boostedAnalysis_ntuples-Legacy_2016_2017_2018_cfg_UL_btag.py'
+config.JobType.psetName = '/work/SM_TTHH/Summer20UL/EL8/Ntuple/CMSSW_10_6_29/src/BoostedTTH/BoostedAnalyzer/test/boostedAnalysis_ntuples-Legacy_2016_2017_2018_cfg_UL_btag.py'
 # config.JobType.outputFiles = ["ntuples_nominal_Tree.root", "ntuples_nominal_Cutflow.txt"]
 
 config.JobType.outputFiles = ["ntuples_nominal_Tree.root", "ntuples_nominal_Cutflow.txt", "ntuples_JESup_Tree.root", "ntuples_JESup_Cutflow.txt", "ntuples_JESdown_Tree.root", "ntuples_JESdown_Cutflow.txt", "ntuples_JERup_Tree.root", "ntuples_JERup_Cutflow.txt", "ntuples_JERdown_Tree.root", "ntuples_JERdown_Cutflow.txt"]
 
-config.JobType.maxJobRuntimeMin = 2750
-config.JobType.maxMemoryMB = 20000
-config.JobType.numCores = 8
+config.JobType.maxJobRuntimeMin = 2000
+config.JobType.maxMemoryMB = 10000
+config.JobType.numCores = 4
 # config.JobType.maxMemoryMB = 4000
 
 config.JobType.pyCfgParams = ['isData=FALSE', 'maxEvents=999999999', 'outName=ntuples', 'dataEra=2018',
