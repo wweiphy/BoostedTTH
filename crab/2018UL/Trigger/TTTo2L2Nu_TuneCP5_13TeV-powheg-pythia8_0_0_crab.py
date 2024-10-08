45bee340393acb8c74eb30907a3fe57e98e88ca0
wweiphy <wei.w@cern.ch>
2024-10-08 16:02:39 -0700

    Update

--- crab/2018UL/Trigger/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8_0_0_crab.py
+++ crab/2018UL/Trigger/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8_0_0_crab.py
@@ -1,16 +1,17 @@
 from CRABClient.UserUtilities import config
 config = config()
 
-config.General.requestName = 'TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8_2018_ntuple_0_0_6'
+config.General.requestName = 'TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8_2018_ntuple_0_0_Trigger'
 config.General.workArea = 'crab_ntuple'
 
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = '/uscms/home/wwei/nobackup/SM_TTHH/Summer20UL/CMSSW_10_6_29/src/BoostedTTH/BoostedAnalyzer/test/boostedAnalysis_ntuples-Legacy_2016_2017_2018_cfg_UL_Trigger.py'
+config.JobType.psetName = '/work/SM_TTHH/Summer20UL/EL8/Ntuple/CMSSW_10_6_29/src/BoostedTTH/BoostedAnalyzer/test/boostedAnalysis_ntuples-Legacy_2016_2017_2018_cfg_UL_Trigger.py'
+
 # config.JobType.outputFiles = ["ntuples_nominal_Tree.root", "ntuples_nominal_Cutflow.txt"]
 
 config.JobType.outputFiles = ["ntuples_nominal_Tree.root", "ntuples_nominal_Cutflow.txt"]
 
-config.JobType.maxJobRuntimeMin = 2750
+config.JobType.maxJobRuntimeMin = 2000
 config.JobType.maxMemoryMB = 20000
 config.JobType.numCores = 8
 # config.JobType.maxMemoryMB = 4000
@@ -22,7 +23,7 @@ config.JobType.numCores = 8
 config.JobType.pyCfgParams = ['isData=FALSE', 'maxEvents=99999999', 'outName=ntuples', 'dataEra=2018',
                               'systematicVariations=nominal', 'weight=2.52E-05', 'ProduceMemNtuples=False', 'deterministicSeeds=False']
 # config.JobType.sendPythonFolder=True
-config.JobType.allowUndistributedCMSSW = True
+# config.JobType.allowUndistributedCMSSW = True
 
 config.Data.inputDataset = '/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL18MiniAODv2-106X_upgrade2018_realistic_v16_L1v1-v1/MINIAODSIM'
 
@@ -31,10 +32,10 @@ config.Data.inputDBS = 'global'
 # config.Data.splitting = 'EventAwareLumiBased'
 # config.Data.unitsPerJob = 5000
 
-NJOB = 341 
+# NJOB = 341 
 config.Data.splitting = 'FileBased'
-config.Data.unitsPerJob = 3
-config.Data.totalUnits = config.Data.unitsPerJob * NJOB
+config.Data.unitsPerJob = 8
+# config.Data.totalUnits = config.Data.unitsPerJob * NJOB
 # config.Data.splitting = 'Automatic'
 config.Data.publication = False
 config.Data.publishDBS = 'phys03'
